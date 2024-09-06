bc0ba5bf17626b4c46460c6211dd90aac609f84f
atevilson <atevilson.araujo@atak.com.br>
2024-09-06 08:08:48 -0300

    update

--- src/category/tests/domain/test_entities.py
+++
@@ -1,9 +0,0 @@
-from category.domain.entities import Category
-import unittest
-
-class TestCategory(unittest.TestCase):
-    def test_constructor(self):
-        category = Category('Nome Sobrenome', 'nome.sobrenome@gmail.com', '99999999999')
-        self.assertEqual(category.name, 'Nome Sobrenome')
-        self.assertEqual(category.email, 'nome.sobrenome@gmail.com')
-        self.assertEqual(category.phone, '99999999999')
